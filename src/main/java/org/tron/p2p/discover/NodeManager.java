0c35d1a68851a4c16c28a2907f8ff25c43919912
jiangyuanshu <317787106@qq.com>
2024-07-18 18:30:47 +0800

    init commit

--- src/main/java/org/tron/p2p/discover/NodeManager.java
+++ src/main/java/org/tron/p2p/discover/NodeManager.java
@@ -1,14 +1,23 @@
 package org.tron.p2p.discover;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.tron.p2p.base.Parameter;
 import org.tron.p2p.discover.protocol.kad.KadService;
 import org.tron.p2p.discover.socket.DiscoverServer;
 
+@Slf4j(topic = "net")
 public class NodeManager {
 
   private static DiscoverService discoverService;
   private static DiscoverServer discoverServer;
+
+  static ScheduledExecutorService testServer = Executors.newSingleThreadScheduledExecutor(
+      new BasicThreadFactory.Builder().namingPattern("testServer").build());
 
   public static void init() {
     discoverService = new KadService();
@@ -16,6 +25,16 @@ public class NodeManager {
     if (Parameter.p2pConfig.isDiscoverEnable()) {
       discoverServer = new DiscoverServer();
       discoverServer.init(discoverService);
+
+      testServer.scheduleWithFixedDelay(() -> {
+        try {
+          for (Node node : getConnectableNodes()) {
+            log.info("ConnectableNode: {}:{}", node.hostV4, node.getPort());
+          }
+        } catch (Exception e) {
+          log.error("DisconnectRandom node failed", e);
+        }
+      }, 30, 10, TimeUnit.SECONDS);
     }
   }
 
